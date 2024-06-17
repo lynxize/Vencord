51f62e1a20409b54dd7369a25168f785333cc815
lynxize <lynxize@gmail.com>
2024-06-17 14:54:32 -0600

    don't identify by nickname only

--- src/userplugins/pk4v/index.tsx
+++ src/userplugins/pk4v/index.tsx
@@ -3,7 +3,7 @@ import { addButton, removeButton } from "@api/MessagePopover";
 import { definePluginSettings } from "@api/Settings";
 import { useAwaiter } from "@utils/react";
 import definePlugin, { OptionType } from "@utils/types";
-import { ChannelStore, GuildMemberStore, MessageActions, MessageStore } from "@webpack/common";
+import { ChannelStore, GuildMemberStore, MessageActions, MessageStore, UserStore } from "@webpack/common";
 
 
 // Inspired By:
@@ -18,8 +18,6 @@ import { ChannelStore, GuildMemberStore, MessageActions, MessageStore } from "@w
 // - Optionally colors member names with either member color, system color, or account role color
 
 // Known Issues:
-// - pk edit button appears for all pk proxied messages - probably unfixable because of api ratelimit
-// - pk users with identical display names will share the same color - probably unfixable because of api ratelimit
 // - pk edit button doesn't quite match normal discord
 // - up arrow to edit most recent message doesn't work
 // - seems to conflict with showMeYourName, which makes sense because the patch is basically the same
@@ -28,6 +26,8 @@ import { ChannelStore, GuildMemberStore, MessageActions, MessageStore } from "@w
 // - Option to enforce minimum hsv value for colors (for readability)
 // - Remove/replace bot/app tag with either "pk" or nothing
 // - Maybe make clicking on profiles work? Not sure if it's possible
+
+const PLURALKIT_BOT_ID = "466378653216014359";
 
 const settings = definePluginSettings({
     colorMode: {
@@ -70,17 +70,18 @@ export default definePlugin({
     ],
 
     renderUsername: ({ author, message }) => useAwaiter(async () => {
-        if (!isPkProxiedMessage(message.channel_id, message.id) || settings.store.colorMode === "None") return <>{author?.nick}</>;
+        const msg: MessageInfo = { channelId: message.getChannelId(), messageId: message.id };
+        if (!isPkProxiedMessage(msg) || settings.store.colorMode === "None") return <>{author?.nick}</>;
 
-        if (!colors[message.channel_id]) colors[message.channel_id] = new Map();
+        const authorId = getAuthorIdentifier(msg);
 
-        var c = colors[message.channel_id][author.nick];
-        if (!c || c.expires < Date.now()) colorsToGet.push([message.channel_id, message.id, author.nick]);
+        let c: NameColor = colors[authorId];
+        if (!c || c.expires < Date.now()) colorsToGet.push(msg);
 
         while (!c) {
             // wait around until it gets around to fetching the color we want
-            await new Promise(r => setTimeout(r, 500));
-            c = colors[message.channel_id][author.nick];
+            await sleep(500);
+            c = colors[authorId];
         }
 
         return <span style={{ color: c.color }}>{author.nick}</span>;
@@ -93,9 +94,7 @@ export default definePlugin({
         setInterval(clearExpiredColors, 1000 * 60 * 5);
 
         addButton("PkEdit", msg => {
-            // this doesn't check if its *your* pk message
-            // not sure how to avoid that without running into pk api ratelimiting issues
-            if (!msg || !isPkProxiedMessage(msg.channel_id, msg.id)) return null;
+            if (!msg || !isOwnPkMessage({ channelId: msg.getChannelId(), messageId: msg.id })) return null;
 
             const handleClick = () => MessageActions.startEditMessage(msg.channel_id, msg.id, msg.content);
 
@@ -112,7 +111,7 @@ export default definePlugin({
 
 
         this.preEditListener = addPreEditListener((channelId, messageId, messageObj) => {
-            if (isPkProxiedMessage(channelId, messageId)) {
+            if (isPkProxiedMessage({ channelId, messageId })) {
                 const { guild_id } = ChannelStore.getChannel(channelId);
                 MessageActions.sendMessage(channelId, {
                     reaction: false,
@@ -131,52 +130,56 @@ export default definePlugin({
     }
 });
 
-// holds an expiry time so that we periodically update colors (for instance, if someone changed their member color)
-class NameColor {
-    expires: number;
-    color: string;
-
-    constructor(color: string, expires: number) {
-        this.expires = expires;
-        this.color = color;
-    }
-}
-
-
-const colorsToGet = new Array<[string, string, string]>(); // channel id, message id, nickname
-const colors = new Map<string, Map<string, NameColor>>(); // channel: (nickname: color)
+type AuthorIdentifier = string;
+const colorsToGet = new Array<MessageInfo>();
+const ownMembers = new Set<AuthorIdentifier>();
+const colors = new Map<AuthorIdentifier, NameColor>();
 
 // this loops forever, getting colors as fast as we can without running
 // into the pk api ratelimit of 2 requests per second
 // it's not great, but it works
 async function fetchColors() {
+    // noinspection InfiniteLoopJS
     while (true) {
         if (colorsToGet.length === 0) {
-            await new Promise(r => setTimeout(r, 100));
+            await sleep(200);
             continue;
         }
+        const message = colorsToGet.pop()!!;
+        const authorId = getAuthorIdentifier(message);
 
-        const [channelId, messageId, nick] = colorsToGet.pop()!!;
-
-        const existing = colors[channelId][messageId];
+        const existing = colors[authorId];
         if (existing && existing.expires > Date.now()) continue; // unexpired one exists, skip
 
 
         // we don't have a color for this nickname already, so hit the pk api
-        const request = await fetch("https://api.pluralkit.me/v2/messages/" + messageId);
+        const request = await fetch("https://api.pluralkit.me/v2/messages/" + message.messageId);
         const json = await request.json();
 
+        if (json.sender === UserStore.getCurrentUser().id) {
+            ownMembers.add(authorId);
+        }
+
         const { colorMode } = settings.store;
-        let color: string | undefined = undefined;
+        let color = "#666666"; // placeholder color
+
         if (colorMode === "Member") color = "#" + json.member?.color;
         else if (colorMode === "System") color = "#" + json.system?.color;
-        else if (colorMode === "Account") color = GuildMemberStore.getMember(ChannelStore.getChannel(channelId).getGuildId(), json.sender)?.colorString;
+        else if (colorMode === "Account") {
+            const account = GuildMemberStore.getMember(
+                ChannelStore.getChannel(message.channelId).getGuildId(),
+                json.sender
+            );
+            color = account?.colorString;
+        }
 
-        color = color ?? "#666666"; // something went wrong
 
-        colors[channelId][nick] = new NameColor(color, Date.now() + 120 * 1000); // expires two minutes from now
+        colors[authorId] = {
+            color: color,
+            expires: Date.now() + 120 * 1000,
+        }; // expires two minutes from now
 
-        await new Promise(r => setTimeout(r, 500)); // we don't want to do more than 2 requests per second
+        await sleep(500); // we don't want to do more than 2 requests per second
     }
 }
 
@@ -185,21 +188,46 @@ async function fetchColors() {
 // just to prevent them growing infinitely
 async function clearExpiredColors() {
     const now = Date.now();
-    colors.forEach((value, key) => {
-        for (const nick in value.keys()) {
-            if (value[nick].expires < now) {
-                value.delete(nick);
-            }
+    for (const authorId in colors.keys()) {
+        if (colors[authorId].expires < now) {
+            colors.delete(authorId);
         }
-    });
+    }
 }
 
 
-function isPkProxiedMessage(channelId: string, messageId: string): boolean {
-    const msg = MessageStore.getMessage(channelId, messageId); // monosodium glutamate
-    return msg && msg.applicationId === "466378653216014359" && msg.webhookId !== undefined;
+function isPkProxiedMessage(message: MessageInfo): boolean {
+    const msg = MessageStore.getMessage(message.channelId, message.messageId); // monosodium glutamate
+    return msg && msg.applicationId === PLURALKIT_BOT_ID && msg.webhookId !== undefined;
 }
 
+function isOwnPkMessage(message: MessageInfo): boolean {
+    if (!isPkProxiedMessage(message)) return false;
+    return ownMembers.has(getAuthorIdentifier(message));
+}
+
+async function sleep(millis: number) {
+    await new Promise(r => setTimeout(r, millis));
+}
+
+// provides a way to differentiate between pk users without touching the pk api
+// includes channel id so that the same member in different servers isn't considered to be the same
+// since the account's role color might be different
+function getAuthorIdentifier(message: MessageInfo): AuthorIdentifier {
+    const msg = MessageStore.getMessage(message.channelId, message.messageId);
+    return msg.author.username + msg.author.avatar + msg.channel_id;
+}
+
+
+type NameColor = {
+    expires: number;
+    color: string;
+}
+
+type MessageInfo = {
+    channelId: string,
+    messageId: string,
+}
 
 const EditIcon = () => {
     return <svg role="img" width="18" height="18" fill="none" viewBox="0 0 24 24">
